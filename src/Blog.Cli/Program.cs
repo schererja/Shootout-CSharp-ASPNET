7c9154ab91ade0af42f6bec19e71fa63efe01aad
Jason Scherer <schereja@gmail.com>
2019-10-31 22:00:37 -0500

    Initial revision

---
+++ src/Blog.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Blog.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
